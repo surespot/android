7ad7225bdbe3ffa11b71163c60ea29b0196e4f35
adampatacchiola@gmail.com <adampatacchiola@gmail.com>
2013-01-02 19:48:29 -0700

    toasters

---
+++ src/com/twofours/surespot/SurespotIdentity.java
@@ -0,0 +1,23 @@
+package com.twofours.surespot;
+
+import java.security.KeyPair;
+
+public class SurespotIdentity {
+	private String mUsername;
+	private KeyPair mKeyPair;
+	
+	
+	
+	public SurespotIdentity(String username, KeyPair keyPair) {		
+		this.mUsername = username;
+		this.mKeyPair = keyPair;
+	}
+	public String getUsername() {
+		return mUsername;
+	}
+	
+	public KeyPair getKeyPair() {
+		return mKeyPair;
+	}
+	
+}
