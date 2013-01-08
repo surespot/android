1b2ea021965b3e5ef51878065cdc5d29d801e122
Adam Patacchiola <adam@2fours.com>
2013-01-08 16:00:35 -0700

    and the multi progress dialog itself

---
+++ src/com/twofours/surespot/MultiProgressDialog.java
@@ -0,0 +1,67 @@
+package com.twofours.surespot;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.Handler;
+
+public class MultiProgressDialog {
+	private int mProgressCounter;
+	private ProgressDialog mMultiProgressDialog;
+	private Context mContext;
+	private String mMessage;
+	private int mDelay;
+
+	public MultiProgressDialog(Context context, String message, int delay) {
+		mProgressCounter = 0;
+		mContext = context;
+		mMessage = message;
+		mDelay = delay;
+	}
+
+	public void incrProgress() {
+		mProgressCounter++;
+		if (mProgressCounter == 1) {
+
+			if (mMultiProgressDialog == null) {
+				mMultiProgressDialog = new ProgressDialog(mContext);
+				mMultiProgressDialog.setIndeterminate(true);
+				// progressDialog.setTitle("loading");
+				mMultiProgressDialog.setMessage(mMessage);
+			}
+
+			// only show the dialog if we haven't loaded within 500 ms
+			Timer timer = new Timer();
+			timer.schedule(new TimerTask() {
+
+				@Override
+				public void run() {
+
+					new Handler(mContext.getMainLooper()).post(new Runnable() {
+
+						@Override
+						public void run() {
+							if (mProgressCounter > 0) {
+								mMultiProgressDialog.show();
+							}
+						}
+					});
+
+				}
+			}, mDelay);
+
+		}
+	}
+
+	public void decrProgress() {
+		mProgressCounter--;
+		if (mProgressCounter == 0) {
+			if (mMultiProgressDialog.isShowing()) {
+				mMultiProgressDialog.dismiss();
+			}
+		}
+	}
+
+}
