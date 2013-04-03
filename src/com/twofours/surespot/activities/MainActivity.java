a6b881f17c2ae5b745243d55944eee23c1a1f0f2
Adam Patacchiola <adam@2fours.com>
2013-04-03 00:13:32 -0600

    check for null image capture handler

--- src/com/twofours/surespot/activities/MainActivity.java
+++ src/com/twofours/surespot/activities/MainActivity.java
@@ -285,8 +285,10 @@ public class MainActivity extends SherlockFragmentActivity {
 			break;
 		case SurespotConstants.IntentRequestCodes.REQUEST_CAPTURE_IMAGE:
 			if (resultCode == RESULT_OK) {
-				//TODO handle null exception by saving state
-				mImageCaptureHandler.handleResult();
+				// TODO handle null exception by saving state
+				if (mImageCaptureHandler != null) {
+					mImageCaptureHandler.handleResult();
+				}
 			}
 			break;
 		}
