3ca3427d7f6344d9750ccf4ed8261a8ddbba6620
Adam Patacchiola <adam@2fours.com>
2013-04-16 16:36:23 -0600

    rip out smelly lingering image crap

--- src/com/twofours/surespot/chat/TextMessageMenuFragment.java
+++ src/com/twofours/surespot/chat/TextMessageMenuFragment.java
@@ -1,20 +1,14 @@
 package com.twofours.surespot.chat;
 
-import java.util.Observable;
-import java.util.Observer;
-
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.DialogInterface.OnShowListener;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.widget.ListView;
 
 import com.actionbarsherlock.app.SherlockDialogFragment;
 import com.twofours.surespot.activities.MainActivity;
-import com.twofours.surespot.common.FileUtils;
 import com.twofours.surespot.identity.IdentityController;
 import com.twofours.surespot.network.IAsyncCallback;
 import com.twofours.surespot.ui.UIUtils;
@@ -24,25 +18,10 @@ public class TextMessageMenuFragment extends SherlockDialogFragment {
 	private SurespotMessage mMessage;
 	private MainActivity mActivity;
 	private String[] mMenuItemArray;
-	private Observer mMessageObserver;
-	private boolean mMyMessage;
-	private boolean mDeleteOnly;
 
 	public void setActivityAndMessage(MainActivity activity, SurespotMessage message) {
 		mMessage = message;
 		mActivity = activity;
-	}
-
-	private void setButtonVisibility() {
-		if (mDeleteOnly) {
-			return;
-		}
-		AlertDialog dialog = (AlertDialog) TextMessageMenuFragment.this.getDialog();
-		ListView listview = dialog.getListView();
-
-		if (!mMyMessage) {
-			listview.getChildAt(0).setEnabled(mMessage.isShareable() && FileUtils.isExternalStorageMounted());
-		}
 	}
 
 	@Override
@@ -57,16 +36,7 @@ public class TextMessageMenuFragment extends SherlockDialogFragment {
 			public void onClick(final DialogInterface dialogi, int which) {
 				if (mMessage == null)
 					return;
-
-				mMessage.deleteObservers();
-
-				AlertDialog dialog = (AlertDialog) TextMessageMenuFragment.this.getDialog();
-				ListView listview = dialog.getListView();
-
-				if (!listview.getChildAt(which).isEnabled()) {
-					return;
-				}
-
+				
 				SharedPreferences sp = mActivity.getSharedPreferences(IdentityController.getLoggedInUser(), Context.MODE_PRIVATE);
 				boolean confirm = sp.getBoolean("pref_delete_message", true);
 				if (confirm) {
@@ -90,27 +60,6 @@ public class TextMessageMenuFragment extends SherlockDialogFragment {
 		});
 
 		AlertDialog dialog = builder.create();
-		dialog.setOnShowListener(new OnShowListener() {
-
-			@Override
-			public void onShow(DialogInterface dialog) {
-				setButtonVisibility();
-
-			}
-		});
-
-		// TODO listen to message control events and handle delete as well
-		mMessageObserver = new Observer() {
-
-			@Override
-			public void update(Observable observable, Object data) {
-				setButtonVisibility();
-
-			}
-		};
-		if (mMessage != null) {
-			mMessage.addObserver(mMessageObserver);
-		}
 		return dialog;
 	}
 
