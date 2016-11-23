39bb81ff4f113398137e3c771ef1cf8dac19e29f
Adam Patacchiola <adam@surespot.me>
2016-11-23 09:06:10 -0700

    working emoji selection

--- libs/emojicon/src/main/java/com/rockerhieu/emojicon/EmojiconsView.java
+++ libs/emojicon/src/main/java/com/rockerhieu/emojicon/EmojiconsView.java
@@ -16,26 +16,14 @@
 
 package com.rockerhieu.emojicon;
 
-import java.util.Arrays;
-import java.util.List;
-
-import com.rockerhieu.emojicon.emoji.Cars;
-import com.rockerhieu.emojicon.emoji.Electronics;
-import com.rockerhieu.emojicon.emoji.Emojicon;
-import com.rockerhieu.emojicon.emoji.Food;
-import com.rockerhieu.emojicon.emoji.Nature;
-import com.rockerhieu.emojicon.emoji.Objects;
-import com.rockerhieu.emojicon.emoji.People;
-import com.rockerhieu.emojicon.emoji.Places;
-import com.rockerhieu.emojicon.emoji.Sport;
-import com.rockerhieu.emojicon.emoji.Symbols;
-
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.database.DataSetObserver;
+import android.graphics.PorterDuff;
 import android.os.Build;
 import android.os.Handler;
 import android.os.SystemClock;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
@@ -44,7 +32,20 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
+
+import com.rockerhieu.emojicon.emoji.Cars;
+import com.rockerhieu.emojicon.emoji.Electronics;
+import com.rockerhieu.emojicon.emoji.Emojicon;
+import com.rockerhieu.emojicon.emoji.Food;
+import com.rockerhieu.emojicon.emoji.Nature;
+import com.rockerhieu.emojicon.emoji.People;
+import com.rockerhieu.emojicon.emoji.Sport;
+import com.rockerhieu.emojicon.emoji.Symbols;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author Daniele Ricci (daniele.athome@gmail.com).
@@ -52,10 +53,11 @@ import android.widget.RelativeLayout;
 public class EmojiconsView extends RelativeLayout implements ViewPager.OnPageChangeListener, EmojiconRecents {
     private OnEmojiconBackspaceClickedListener mOnEmojiconBackspaceClickedListener;
     private int mEmojiTabLastSelectedIndex = -1;
-    private View[] mEmojiTabs;
+    private ImageView[] mEmojiTabs;
     private EmojisPagerAdapter mEmojisAdapter;
     private EmojiconRecentsManager mRecentsManager;
     private boolean mUseSystemDefault = false;
+    private int mThemeAccentColor;
 
     public EmojiconsView(Context context) {
         super(context);
@@ -83,6 +85,14 @@ public class EmojiconsView extends RelativeLayout implements ViewPager.OnPageCha
         // we handle recents
         EmojiconRecents recents = this;
 
+
+
+//
+//        final TypedValue value = new TypedValue();
+//
+//        getContext().getTheme().resolveAttribute(R.attr.colorAccent, value, true);
+        mThemeAccentColor = ContextCompat.getColor(getContext(), R.color.surespotBlue);
+
         mEmojisAdapter = new EmojisPagerAdapter(Arrays.asList(
                 EmojiconRecentsGridView.newInstance(getContext(), this, mUseSystemDefault),
                 EmojiconGridView.newInstance(getContext(), this, People.DATA, recents, mUseSystemDefault),
@@ -95,15 +105,15 @@ public class EmojiconsView extends RelativeLayout implements ViewPager.OnPageCha
         ));
         emojisPager.setAdapter(mEmojisAdapter);
 
-        mEmojiTabs = new View[8];
-        mEmojiTabs[0] = findViewById(R.id.emojis_tab_0_recents);
-        mEmojiTabs[1] = findViewById(R.id.emojis_tab_1_people);
-        mEmojiTabs[2] = findViewById(R.id.emojis_tab_2_nature);
-        mEmojiTabs[3] = findViewById(R.id.emojis_tab_3_food);
-        mEmojiTabs[4] = findViewById(R.id.emojis_tab_4_sport);
-        mEmojiTabs[5] = findViewById(R.id.emojis_tab_5_cars);
-        mEmojiTabs[6] = findViewById(R.id.emojis_tab_6_electronics);
-        mEmojiTabs[7] = findViewById(R.id.emojis_tab_7_symbols);
+        mEmojiTabs = new ImageView[8];
+        mEmojiTabs[0] = (ImageView) findViewById(R.id.emojis_tab_0_recents);
+        mEmojiTabs[1] = (ImageView) findViewById(R.id.emojis_tab_1_people);
+        mEmojiTabs[2] = (ImageView) findViewById(R.id.emojis_tab_2_nature);
+        mEmojiTabs[3] = (ImageView) findViewById(R.id.emojis_tab_3_food);
+        mEmojiTabs[4] = (ImageView) findViewById(R.id.emojis_tab_4_sport);
+        mEmojiTabs[5] = (ImageView) findViewById(R.id.emojis_tab_5_cars);
+        mEmojiTabs[6] = (ImageView) findViewById(R.id.emojis_tab_6_electronics);
+        mEmojiTabs[7] = (ImageView) findViewById(R.id.emojis_tab_7_symbols);
         for (int i = 0; i < mEmojiTabs.length; i++) {
             final int position = i;
             mEmojiTabs[i].setOnClickListener(new View.OnClickListener() {
@@ -193,10 +203,15 @@ public class EmojiconsView extends RelativeLayout implements ViewPager.OnPageCha
             case 3:
             case 4:
             case 5:
+            case 6:
+            case 7:
+
                 if (mEmojiTabLastSelectedIndex >= 0 && mEmojiTabLastSelectedIndex < mEmojiTabs.length) {
                     mEmojiTabs[mEmojiTabLastSelectedIndex].setSelected(false);
+                    mEmojiTabs[mEmojiTabLastSelectedIndex].clearColorFilter();
                 }
                 mEmojiTabs[i].setSelected(true);
+                mEmojiTabs[i].setColorFilter(mThemeAccentColor, PorterDuff.Mode.SRC_IN);
                 mEmojiTabLastSelectedIndex = i;
                 mRecentsManager.setRecentPage(i);
                 break;
