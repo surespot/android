660d107ef5277822727c7c67b158fcd81b674981
Adam Patacchiola <adam@surespot.me>
2017-05-14 21:50:15 -0600

    don't use clearable edit text.

--- libs/emojicon/src/main/java/com/rockerhieu/emojicon/EmojiconEditText.java
+++ libs/emojicon/src/main/java/com/rockerhieu/emojicon/EmojiconEditText.java
@@ -18,17 +18,13 @@ package com.rockerhieu.emojicon;
 
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.graphics.drawable.Drawable;
-import android.text.TextUtils;
 import android.util.AttributeSet;
-import android.view.MotionEvent;
-import android.view.View;
 import android.widget.EditText;
 
 /**
  * @author Hieu Rocker (rockerhieu@gmail.com).
  */
-public class EmojiconEditText extends EditText implements View.OnTouchListener, View.OnFocusChangeListener {
+public class EmojiconEditText extends EditText {
     private int mEmojiconSize;
     private boolean mUseSystemDefault = false;
 
@@ -48,10 +44,6 @@ public class EmojiconEditText extends EditText implements View.OnTouchListener, 
     }
 
     private void init(AttributeSet attrs) {
-        super.setOnTouchListener(this);
-        super.setOnFocusChangeListener(this);
-        initIcon();
-        setClearIconVisible(false);
         TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.Emojicon);
         mEmojiconSize = (int) a.getDimension(R.styleable.Emojicon_emojiconSize, getTextSize());
         mUseSystemDefault = a.getBoolean(R.styleable.Emojicon_emojiconUseSystemDefault, false);
@@ -62,9 +54,6 @@ public class EmojiconEditText extends EditText implements View.OnTouchListener, 
     @Override
     protected void onTextChanged(CharSequence text, int start, int lengthBefore, int lengthAfter) {
         updateText(start, lengthAfter, false);
-        if (isFocused()) {
-            setClearIconVisible(!TextUtils.isEmpty(text));
-        }
     }
 
     /**
@@ -89,127 +78,5 @@ public class EmojiconEditText extends EditText implements View.OnTouchListener, 
      */
     public void setUseSystemDefault(boolean useSystemDefault) {
         mUseSystemDefault = useSystemDefault;
-    }
-
-    public static enum Location {
-        LEFT(0), RIGHT(2);
-
-        final int idx;
-
-        private Location(int idx) {
-            this.idx = idx;
-        }
-    }
-
-    public interface Listener {
-        void didClearText();
-    }
-
-
-
-    public void setListener(Listener listener) {
-        this.listener = listener;
-    }
-
-    /**
-     * null disables the icon
-     */
-    public void setIconLocation(Location loc) {
-        this.loc = loc;
-        initIcon();
-    }
-
-    @Override
-    public void setOnTouchListener(OnTouchListener l) {
-        this.l = l;
-    }
-
-    @Override
-    public void setOnFocusChangeListener(OnFocusChangeListener f) {
-        this.f = f;
-    }
-
-    private Location loc = Location.RIGHT;
-
-    private Drawable xD;
-    private Listener listener;
-
-    private OnTouchListener l;
-    private OnFocusChangeListener f;
-
-    @Override
-    public boolean onTouch(View v, MotionEvent event) {
-        if (getDisplayedDrawable() != null) {
-            int x = (int) event.getX();
-            int y = (int) event.getY();
-            int left = (loc == Location.LEFT) ? 0 : getWidth() - getPaddingRight() - xD.getIntrinsicWidth();
-            int right = (loc == Location.LEFT) ? getPaddingLeft() + xD.getIntrinsicWidth() : getWidth();
-            boolean tappedX = x >= left && x <= right && y >= 0 && y <= (getBottom() - getTop());
-            if (tappedX) {
-                if (event.getAction() == MotionEvent.ACTION_UP) {
-                    setText("");
-                    if (listener != null) {
-                        listener.didClearText();
-                    }
-                }
-                return true;
-            }
-        }
-        if (l != null) {
-            return l.onTouch(v, event);
-        }
-        return false;
-    }
-
-    @Override
-    public void onFocusChange(View v, boolean hasFocus) {
-        if (hasFocus) {
-            setClearIconVisible(!TextUtils.isEmpty(getText()));
-        } else {
-            setClearIconVisible(false);
-        }
-        if (f != null) {
-            f.onFocusChange(v, hasFocus);
-        }
-    }
-
-
-
-    @Override
-    public void setCompoundDrawables(Drawable left, Drawable top, Drawable right, Drawable bottom) {
-        super.setCompoundDrawables(left, top, right, bottom);
-      //  initIcon();
-    }
-
-
-
-    private void initIcon() {
-        xD = null;
-        if (loc != null) {
-            xD = getCompoundDrawables()[loc.idx];
-        }
-        if (xD == null) {
-            xD = getResources().getDrawable(android.R.drawable.presence_offline);
-        }
-        xD.setBounds(0, 0, xD.getIntrinsicWidth(), xD.getIntrinsicHeight());
-        int min = getPaddingTop() + xD.getIntrinsicHeight() + getPaddingBottom();
-        if (getSuggestedMinimumHeight() < min) {
-            setMinimumHeight(min);
-        }
-    }
-
-    private Drawable getDisplayedDrawable() {
-        return (loc != null) ? getCompoundDrawables()[loc.idx] : null;
-    }
-
-    protected void setClearIconVisible(boolean visible) {
-        Drawable[] cd = getCompoundDrawables();
-        Drawable displayed = getDisplayedDrawable();
-        boolean wasVisible = (displayed != null);
-        if (visible != wasVisible) {
-            Drawable x = visible ? xD : null;
-            super.setCompoundDrawables((loc == Location.LEFT) ? x : cd[0], cd[1], (loc == Location.RIGHT) ? x : cd[2],
-                    cd[3]);
-        }
     }
 }
